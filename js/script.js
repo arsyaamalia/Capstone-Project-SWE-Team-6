c9f0adf320499795477d1c45f79fb3170bc984bd
Arsya Amalia <arsyaamalia1@gmail.com>
2023-06-19 00:26:19 +0700

    arsya bikin halaman about

--- js/script.js
+++ js/script.js
@@ -26,16 +26,18 @@ function showLoggedInView(user) {
   
     loginOkElement.style.display = 'block';
     loginNokElement.style.display = 'none';
-    userContainer.textContent = ', ' + user.username;
+    userContainer.textContent = ', ' + user.name;
     imgElement.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png';
-    imgElement.className = 'width-40 margin-left-10';
+    imgElement.className = 'avatar-img width-40';
   }
   
   // Function to show view for logged out user
   function showLoggedOutView() {
+    var loginOkkElement = document.getElementById('login-okk');
     var loginOkElement = document.getElementById('login-ok');
     var loginNokElement = document.getElementById('login-nok');
-  
+    
+    loginOkkElement.style.display = 'none';
     loginOkElement.style.display = 'none';
     loginNokElement.style.display = 'block';
   }
